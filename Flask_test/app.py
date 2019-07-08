a65ba6c00b87d5dc34f47eb508d1e7af495445ed
jaymondyu <18875001129@163.com>
2019-07-08 16:12:42 +0800

    22
    22

--- Flask_test/app.py
+++ Flask_test/app.py
@@ -1,11 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import flask
+import mysql.connector
+import json
+
+
 
 app = flask.Flask(__name__)
+
+conn=mysql.connector.connect(user="root",password="plokijuh9",database="j")#连接数据库，创建Flask_app数据库
+cursor=conn.cursor()
 
 
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+@app.route('/search/',methods=['POST'])
+def search():
+    Gene = flask.request.values['genes']
+    sql= "select * FROM illuminamethyl450_hg19_gpl16304_tcgalegacy where gene LIKE '%" + Gene + "%'"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    # for data in
+    print(type(result))
+    return json.dumps(result)
 
 @app.route('/Tool/')
 def Tool():
